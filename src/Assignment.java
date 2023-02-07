a64b0e26722d927eb6f9760a33088b83e471a89d
saikiranso <210303126185@paruluniversity.ac.in>
2023-02-07 15:58:02 +0530

    Initial commit

---
+++ src/Assignment.java
@@ -0,0 +1,10 @@
+public class Assignment {
+    public static void main(String[] args) {
+        String a = "nakka";
+        a.concat("is a puka");
+        System.out.println(a);
+        a = a.concat(" fuck u");
+        System.out.println(a);
+
+    }
+}
